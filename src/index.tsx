f2322b026381fa50db4fd99ec24a2d86ead2d4f7
Ryan Gilbert <ryan.gilbert@formidable.com>
2020-11-14 19:25:43 -0500

    Set up urql, styled components, and react router dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createClient, Provider } from 'urql';
+import { createGlobalStyle, DefaultTheme, ThemeProvider } from 'styled-components';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const client = createClient({
+  url: 'https://threed-test-api.herokuapp.com/graphql',
+});
+
+export const darkTheme: DefaultTheme = {
+  colors: {
+    background: '#212429',
+    text: '#ffffff',
+  },
+};
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: ${(p) => p.theme.colors.background};
+    color: ${(p) => p.theme.colors.text};
+
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    font-size: 1rem;
+    }
+`;
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider value={client}>
+      <ThemeProvider theme={darkTheme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
